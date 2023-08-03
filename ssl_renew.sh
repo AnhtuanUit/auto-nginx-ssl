dd64b68b23ddc71ad8524362d67263ab6d88a3bf
AnhtuanUit <anhtuan7194@gmail.com>
2023-08-03 10:56:05 +0700

    Update project path config

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --ansi never"
 DOCKER="/usr/bin/docker"
 
-cd /root/node_project/
+cd /root/auto-nginx-ssl/
 $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
